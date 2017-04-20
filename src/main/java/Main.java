998f4e2993384baa5be5f087be234e7481150559
adem <adem@adem-Latitude-E5400>
2017-04-20 16:15:50 +0200

    Update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,11 @@
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 import yahoofinance.Stock;
 import yahoofinance.YahooFinance;
+import yahoofinance.histquotes.HistoricalQuote;
 import yahoofinance.histquotes.Interval;
 
 public class Main {
@@ -11,12 +13,14 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		Calendar from = Calendar.getInstance();
 		Calendar to = Calendar.getInstance();
-		from.add(Calendar.YEAR, -5); // from 5 years ago
+		from.add(Calendar.YEAR, -1); // from 5 years ago
+		SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-dd");
+		Stock google = YahooFinance.get("GOOG", from, to, Interval.MONTHLY);
+		for (HistoricalQuote s : google.getHistory()) {
+			String formatted = format1.format(s.getDate().getTime());
 
-		Stock google = YahooFinance.get("GOOG", from, to, Interval.DAILY);
-		
-		System.out.println(google.getHistory());
-
+			System.out.println(formatted + " : " + s.getAdjClose());
+		}
 	}
 
 }
